5e9d875b951b1de1aa1082c223782f20419ed9b0
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2018-06-12 07:44:19 +0000

    ECO-1574: Add requests to facade.

--- src/SprykerEco/Zed/ComputopApi/Business/ComputopApiFacade.php
+++ src/SprykerEco/Zed/ComputopApi/Business/ComputopApiFacade.php
@@ -7,6 +7,9 @@
 
 namespace SprykerEco\Zed\ComputopApi\Business;
 
+use Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer;
+use Generated\Shared\Transfer\OrderTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
 use Spryker\Zed\Kernel\Business\AbstractFacade;
 
 /**
@@ -14,4 +17,123 @@ use Spryker\Zed\Kernel\Business\AbstractFacade;
  */
 class ComputopApiFacade extends AbstractFacade implements ComputopApiFacadeInterface
 {
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiEasyCreditStatusResponseTransfer
+     */
+    public function performEasyCreditStatusRequest(
+        QuoteTransfer $quoteTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createEasyCreditStatusRequest()
+            ->request($quoteTransfer, $computopApiHeaderPayment);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiAuthorizeResponseTransfer
+     */
+    public function performAuthorizationRequest(
+        OrderTransfer $orderTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createAuthorizationPaymentRequest()
+            ->request($orderTransfer, $computopApiHeaderPayment);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiCaptureResponseTransfer
+     */
+    public function performCaptureRequest(
+        OrderTransfer $orderTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createCapturePaymentRequest()
+            ->request($orderTransfer, $computopApiHeaderPayment);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiInquireResponseTransfer
+     */
+    public function performInquireRequest(
+        OrderTransfer $orderTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createInquirePaymentRequest()
+            ->request($orderTransfer, $computopApiHeaderPayment);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiRefundResponseTransfer
+     */
+    public function performRefundRequest(
+        OrderTransfer $orderTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createRefundPaymentRequest()
+            ->request($orderTransfer, $computopApiHeaderPayment);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\OrderTransfer $orderTransfer
+     * @param \Generated\Shared\Transfer\ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+     *
+     * @return \Generated\Shared\Transfer\ComputopApiReverseResponseTransfer
+     */
+    public function performReverseRequest(
+        OrderTransfer $orderTransfer,
+        ComputopApiHeaderPaymentTransfer $computopApiHeaderPayment
+    ) {
+        return $this
+            ->getFactory()
+            ->createReversePaymentRequest()
+            ->request($orderTransfer, $computopApiHeaderPayment);
+    }
 }

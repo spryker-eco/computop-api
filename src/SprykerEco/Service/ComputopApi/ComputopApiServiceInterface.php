3c53422b9f283bf4b489f3de2cfbc20fab35792b
Roman Sadkivskyi <sad.pom@gmail.com>
2021-04-02 16:17:32 +0300

    ECO-3623 / ECO-3661 - Fix travis issue with database creation

--- src/SprykerEco/Service/ComputopApi/ComputopApiServiceInterface.php
+++ src/SprykerEco/Service/ComputopApi/ComputopApiServiceInterface.php
@@ -59,7 +59,7 @@ interface ComputopApiServiceInterface
      * @param string[] $responseArray
      * @param string $key
      *
-     * @return string|null
+     * @return string|null|bool
      */
     public function getResponseValue(array $responseArray, $key);
 

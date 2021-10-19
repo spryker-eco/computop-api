47d2c340493dc6159d51475ccbfa8866de64f1fd
Kostiantyn Kichehlov <kostiantyn.kichehlov@spryker.com>
2021-10-19 08:54:10 +0300

    ECO-3656 Fix CR notices

--- tests/SprykerEcoTest/Zed/ComputopApi/_support/Module/EnvironmentModule.php
+++ tests/SprykerEcoTest/Zed/ComputopApi/_support/Module/EnvironmentModule.php
@@ -13,7 +13,6 @@ use SprykerTest\Shared\Testify\Helper\Environment;
 class EnvironmentModule extends Environment
 {
     /**
-     * @inheritDoc
      *
      * @param \Codeception\Lib\ModuleContainer $moduleContainer
      * @param array|null $config
